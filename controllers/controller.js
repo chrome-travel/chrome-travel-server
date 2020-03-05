f5148aa6d614f20fee9715ca68db1971d74a7612
naufalyunan <naufalyunan45@gmail.com>
2020-03-05 22:25:11 +0700

    add oauth

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,7 @@ const { User } = require('./../models')
 const CustomError = require('./../helpers/customError')
 const { comparePass } = require('./../helpers/bcrypt')
 const { generateToken } = require('./../helpers/jwt')
+const {OAuth2Client} = require('google-auth-library');
 
 class Controller{
     static login(req,res,next){
@@ -43,6 +44,56 @@ class Controller{
                 res.status(500).json(err)
             })
             
+    }
+
+    static loginGoogle(req, res, next) {
+        const idToken = req.headers.id_token
+        const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+        let data
+        // console.log(idtoken)
+        client.verifyIdToken({
+            idToken,
+            audience: process.env.GOOGLE_CLIENT_ID
+        })
+            .then(ticket => {
+                const payload = ticket.getPayload()
+                console.log(payload)
+                const email = payload.email
+                data = payload
+                return User.findAll({
+                    where: {
+                        email
+                    }
+                })
+            })
+            .then(result => {
+                if (result.length === 0) {
+                    return User.create({
+                        name: data.name,
+                        email: data.email,
+                        password: `${data.email}+g`,
+                        gender: true
+                    })
+                } else {
+                    return result[0]
+                }
+                
+            })
+            .then(result => {
+                const newPayload = {
+                    id: result.id,
+                    name: result.name,
+                    email: result.email,
+                    phone_number:result.phone_number,
+                    gender: result.gender
+                }
+                const token = generateToken(newPayload)
+                req.headers.token = token
+                res.status(201).json({ token })
+            })
+            .catch(err => {
+                res.status(err)
+            })
     }
 }
 
