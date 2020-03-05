550340858626b1b3a95c09555c0c9354406f111f
felixwinangun <felixwinangun@ymail.com>
2020-03-06 01:18:36 +0700

    error handling login

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,25 +2,23 @@ const { User } = require('./../models')
 const CustomError = require('./../helpers/customError')
 const { comparePass } = require('./../helpers/bcrypt')
 const { generateToken } = require('./../helpers/jwt')
-const {OAuth2Client} = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
 
-class Controller{
-    static login(req,res,next){
+const invalid = "Email/Password wrong";
+
+class Controller {
+    static login(req, res, next) {
         const { email, password } = req.body
         User.findOne({
             where: {
                 email
             }
-        }) 
+        })
             .then(result => {
                 if (!result) {
-                    const error = new CustomError(404,"Email/Password wrong")
-                    throw(error)
+                    throw new CustomError(400, invalid)
                 } else {
                     const authenticate = comparePass(password, result.dataValues.password)
-                    console.log(password)
-                    console.log(result.dataValues.password)
-                    console.log(authenticate)
                     if (authenticate) {
                         const payload = {
                             id: result.dataValues.id,
@@ -30,32 +28,28 @@ class Controller{
                             gender: result.dataValues.gender
                         }
                         const token = generateToken(payload)
-                        req.headers.token = token 
-                        console.log(token)
+                        req.headers.token = token
                         res.status(200).json({ token })
                     } else {
-                        const error = new CustomError(400,"Email/Password wrong")
-                        throw(error)
+                        throw new CustomError(400, invalid)
                     }
                 }
             })
             .catch(err => {
                 next(err)
-            })    
+            })
     }
 
     static loginGoogle(req, res, next) {
         const idToken = req.headers.id_token
         const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
         let data
-        // console.log(idtoken)
         client.verifyIdToken({
             idToken,
             audience: process.env.GOOGLE_CLIENT_ID
         })
             .then(ticket => {
                 const payload = ticket.getPayload()
-                console.log(payload)
                 const email = payload.email
                 data = payload
                 return User.findAll({
@@ -75,14 +69,13 @@ class Controller{
                 } else {
                     return result[0]
                 }
-                
             })
             .then(result => {
                 const newPayload = {
                     id: result.id,
                     name: result.name,
                     email: result.email,
-                    phone_number:result.phone_number,
+                    phone_number: result.phone_number,
                     gender: result.gender
                 }
                 const token = generateToken(newPayload)
